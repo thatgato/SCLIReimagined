428e583bccf884d607b62cdd11a5d00d3a5a9267
thatgato <thatgatooo@gmail.com>
2025-06-08 23:09:03 +0200

    Other setup

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
+// Copyright (c) 2025. @thatgato
+// This file is a part of SCLIReimagined, which is licensed under the GNU GPLv3 license.
+// See the LICENSE file for more information.
+
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    auto lang1 = "C++";
+    std::cout << "Hello and welcome to " << lang1 << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+
+    for (int i = 1; i <= 5; i++) { std::cout << "i = " << i << std::endl; }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
